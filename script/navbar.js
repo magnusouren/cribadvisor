130a1e73018d88a39a47ce0ee37377543190f3f6
magnusouren <magnut0203@gmail.com>
2021-10-09 00:48:51 +0200

    la til logo-fil og bildemappe, noen endringer i navbar.js

--- script/navbar.js
+++ script/navbar.js
@@ -1,11 +1,14 @@
 const navbar = document.getElementById("navbar");
 
+//lager et ul-element i navbar
 let ulist = document.createElement("ul");
 navbar.appendChild(ulist);
 
-//const listelements = ["Hjem", "Hybler", "Kontkat oss", "Legg til hybel"];
-
 const listelements = [
+  {
+    src="img/others/logo.png",
+    text="CribAdvisor",
+  },
   {
     name: "Hjem",
     link: "index.html",
@@ -24,11 +27,10 @@ const listelements = [
   },
 ];
 
-for (let i = 0; i < listelements.length; i++) {
+for (let i = 1; i < listelements.length; i++) {
   let list = document.createElement("li");
   let a = document.createElement("a");
   let content = document.createTextNode(listelements[i].name);
-  //a.href = "https://www.vg.no";
   a.href = listelements[i].link;
   a.appendChild(content);
   list.appendChild(a);
