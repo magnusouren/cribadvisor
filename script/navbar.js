d2fe4d83991b27441cfb067e61c51e29aa55b5ac
Magnus T. Ouren <91123056+magnusouren@users.noreply.github.com>
2021-10-28 14:01:07 +0200

    Add href on logo (#14)
    * Add href on logo
    
    * Fix removed unnecessary code

--- script/navbar.js
+++ script/navbar.js
@@ -1,28 +1,30 @@
 const navbar = document.getElementById("navbar");
 
 const listelements = [
-    {
-        source: "img/others/logo.png",
-        text: "CribAdvisor",
-    },
-    {
-        name: "Hjem",
-        link: "index.html",
-    },
-    {
-        name: "Hybler",
-        link: "hybler.html",
-    },
-    {
-        name: "Kontakt Oss",
-        link: "kontakt-oss.html",
-    },
-    {
-        name: "Legg til hybel",
-        link: "legg-til-hybel.html",
-    },
+  {
+    source: "img/others/logo.png",
+    text: "CribAdvisor",
+  },
+  {
+    name: "Hjem",
+    link: "index.html",
+  },
+  {
+    name: "Hybler",
+    link: "hybler.html",
+  },
+  {
+    name: "Kontakt Oss",
+    link: "kontakt-oss.html",
+  },
+  {
+    name: "Legg til hybel",
+    link: "legg-til-hybel.html",
+  },
 ];
 //legger til bilde og tekst til nav-bar
+let a = document.createElement("a");
+a.href = "index.html";
 let container = document.createElement("div");
 
 let logo = document.createElement("img");
@@ -34,7 +36,8 @@ let textNode = document.createTextNode(listelements[0].text);
 logoText.appendChild(textNode);
 container.appendChild(logoText);
 
-navbar.appendChild(container);
+a.appendChild(container);
+navbar.appendChild(a);
 
 //lager et ul-element i navbar
 let ulist = document.createElement("ul");
@@ -42,13 +45,13 @@ navbar.appendChild(ulist);
 
 //legger til alle sidene som li-elementer i ul-elementet
 for (let i = 1; i < listelements.length; i++) {
-    let list = document.createElement("li");
-    let a = document.createElement("a");
-    let content = document.createTextNode(listelements[i].name);
-    a.href = listelements[i].link;
-    a.appendChild(content);
-    list.appendChild(a);
-    ulist.appendChild(list);
+  let list = document.createElement("li");
+  let a = document.createElement("a");
+  let content = document.createTextNode(listelements[i].name);
+  a.href = listelements[i].link;
+  a.appendChild(content);
+  list.appendChild(a);
+  ulist.appendChild(list);
 }
 
 //henter filnavnet på aktiv fil
@@ -57,16 +60,12 @@ const pathname = window.location.pathname;
 console.log("pathname", pathname);
 //setter stil på aktiv lenke til mørkere med understrek
 for (let i = 0; i < listelements.length - 1; i++) {
-    let aTagHref = document
-        .getElementById("navbar")
-        .getElementsByTagName("a")
-        [i].getAttribute("href");
-    if (pathname.includes(aTagHref)) {
-        /*document.getElementById("navbar").getElementsByTagName("a")[
-      i
-    ].style.textDecoration = "underline";*/
-        document.getElementById("navbar").getElementsByTagName("a")[
-            i
-        ].style.color = "#253257";
-    }
+  let aTagHref = document
+    .getElementById("navbar")
+    .getElementsByTagName("a")
+    [i].getAttribute("href");
+  if (pathname.includes(aTagHref)) {
+    document.getElementById("navbar").getElementsByTagName("a")[i].style.color =
+      "#253257";
+  }
 }
