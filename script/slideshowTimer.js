dc90cbbd7e9951906837d65faffe325eb55bedf4
magnusouren <magnut0203@gmail.com>
2021-10-28 17:26:14 +0200

    Add timer and fade

--- script/slideshowTimer.js
+++ script/slideshowTimer.js
@@ -2,14 +2,22 @@ const bilder = ["img1.jpg", "img2.jpg", "img3.jpg"];
 const imgSlide = document.getElementById("imgSlide");
 
 var i = 1;
+let x = 1;
 function swapImg() {
-  console.log(i);
   if (i == bilder.length) {
     i = 0;
-  } else {
-    imgSlide.src = "img/test/" + bilder[i];
   }
+  imgSlide.src = "img/test/" + bilder[i];
+  imgSlide.style.opacity = 1;
   i++;
 }
+function fadeOut() {
+  imgSlide.style.opacity = 0.5;
+}
+function fadeInn() {
+  imgSlide.style.opacity = 1;
+}
 
-setInterval(swapImg, 2000);
+setInterval(fadeOut, 5000);
+setTimeout(() => setInterval(swapImg, 5000), 500);
+//setTimeout(fadeInn, 2000);
