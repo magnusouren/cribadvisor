a6a822850b739f3f7f4fa750647e57c4b18cf4ef
magnusouren <magnut0203@gmail.com>
2021-10-28 14:51:33 +0200

    Add swap img on timer

--- script/slideshowTimer.js
+++ script/slideshowTimer.js
@@ -1,8 +1,15 @@
 const bilder = ["img1.jpg", "img2.jpg", "img3.jpg"];
 const imgSlide = document.getElementById("imgSlide");
 
-function swapImg(i) {
-  imgSlide.src = "img/test/" + bilder[i];
+var i = 1;
+function swapImg() {
+  console.log(i);
+  if (i == bilder.length) {
+    i = 0;
+  } else {
+    imgSlide.src = "img/test/" + bilder[i];
+  }
+  i++;
 }
 
-swapImg(1);
+setInterval(swapImg, 2000);
