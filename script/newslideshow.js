ca842125e2670158c4b081a9fa8fd555e7c3ab48
magnusouren <magnut0203@gmail.com>
2021-11-02 14:40:06 +0100

    Add fade inn/out on slideshow

--- script/newslideshow.js
+++ script/newslideshow.js
@@ -3,23 +3,28 @@ import { fetchData } from "./common.js";
 async function createListOfDorms() {
   const data = await fetchData();
 
-  // for (let i = 0; i < data.length; i++) {
-  //   swapImg(data[i]);
-  //   //console.log(data.indexOf(data[i]));
-  // }
   let counter = 0;
-  setInterval(() => {
-    console.log(counter);
-    swapImg(data[counter]);
-    counter++;
-    if (counter == data.length) {
-      counter = 0;
-    }
-  }, 5000);
+  setTimeout(
+    () =>
+      setInterval(() => {
+        console.log(counter);
+        swapImg(data[counter]);
+        counter++;
+        if (counter == data.length) {
+          counter = 0;
+        }
+      }, 5000),
+    800
+  );
 }
 createListOfDorms();
 function swapImg(data) {
   let slideshow = document.getElementById("imgSlide");
   console.log(data.images[0].src);
   slideshow.src = data.images[0].src;
+  imgSlide.style.opacity = 1;
 }
+function fadeOut() {
+  imgSlide.style.opacity = 0.5;
+}
+setInterval(fadeOut, 5000);
