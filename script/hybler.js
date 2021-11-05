f77b15125d0c7a6335a306241283bf0f8683304c
Isak Solheim <42111513+isaksolheim@users.noreply.github.com>
2021-11-05 12:37:15 +0100

    Feature/hybler (#28)

---
+++ script/hybler.js
@@ -0,0 +1,29 @@
+import { fetchData } from './common.js';
+import {
+  createDormImage,
+  createDormTextContent,
+  createButton,
+} from './hyblerComponents.js';
+
+function createDorm(dorm) {
+  const { name, address, capasity, events, about, ratings, images } = dorm;
+
+  let container = document.createElement('div');
+  container.className = 'dorm-container';
+
+  createDormImage(container, images);
+  createDormTextContent(container, name, address, events, ratings);
+  createButton(container);
+
+  document.getElementById('hybler').appendChild(container);
+}
+
+function createListOfDorms() {
+  const data = fetchData();
+
+  for (let i = 0; i < data.length; i++) {
+    createDorm(data[i]);
+  }
+}
+
+createListOfDorms();
