638ef716936e0350b6425a38cffdfd9083c3d8ab
isaksolheim <isak.m.h.s@gmail.com>
2021-11-05 10:55:09 +0100

    Split createDorm() into multiple functions

--- script/hybler.js
+++ script/hybler.js
@@ -1,32 +1,25 @@
 import { fetchData } from './common.js';
 
-async function createListOfDorms() {
-  const data = await fetchData();
-
-  for (let i = 0; i < data.length; i++) {
-    createDorm(data[i]);
-  }
-}
-
-function createDorm(dorm) {
-  const { name, address, capasity, events, about, ratings, images } = dorm;
-
-  let container = document.createElement('div');
-  container.className = 'container';
-
+function createDormImage(container, images) {
   let img = document.createElement('img');
   img.src = images[0].src;
   img.className = 'dorm-image';
   container.appendChild(img);
+}
 
+function createDormName(container, name) {
   let dormName = document.createElement('h2');
   dormName.innerHTML = name;
   container.appendChild(dormName);
+}
 
+function createDormAddress(container, address) {
   let dormAddress = document.createElement('h3');
   dormAddress.innerHTML = address;
   container.appendChild(dormAddress);
+}
 
+function createEventList(container, events) {
   let eventListContainer = document.createElement('div');
   let eventListHeader = document.createElement('p');
   eventListHeader.innerHTML = 'Tilbyr:';
@@ -40,10 +33,40 @@ function createDorm(dorm) {
   }
   eventListContainer.appendChild(eventList);
   container.appendChild(eventListContainer);
+}
 
+function createDormRatings(container, ratings) {
   let dormRatings = document.createElement('p');
   dormRatings.innerHTML = `${ratings.numberOfRatings} vurderinger`;
   container.appendChild(dormRatings);
+}
+
+function createButton(container) {
+  let button = document.createElement('button');
+  button.innerHTML = 'Se Hybel';
+  container.appendChild(button);
+}
+
+function createListOfDorms() {
+  const data = fetchData();
+
+  for (let i = 0; i < data.length; i++) {
+    createDorm(data[i]);
+  }
+}
+
+function createDorm(dorm) {
+  const { name, address, capasity, events, about, ratings, images } = dorm;
+
+  let container = document.createElement('div');
+  container.className = 'container';
+
+  createDormImage(container, images);
+  createDormName(container, name);
+  createDormAddress(container, address);
+  createEventList(container, events);
+  createDormRatings(container, ratings);
+  createButton(container);
 
   document.getElementById('hybler').appendChild(container);
 }
