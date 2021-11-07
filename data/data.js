1f06259fe0417e2e59473b4bb2e7a614b2e73951
magnusouren <magnut0203@gmail.com>
2021-11-07 15:21:19 +0100

    Add data on Tales crib

--- data/data.js
+++ data/data.js
@@ -1,11 +1,11 @@
 export const data = [
   {
-    name: 'Ravens Nest',
-    address: 'Kirkegata 61A',
+    name: "Ravens Nest",
+    address: "Kirkegata 61A",
     capasity: 16,
-    events: ['Fest', 'Grilling', 'Spillkveld'],
+    events: ["Fest", "Grilling", "Spillkveld"],
     about:
-      'YO! Jeg tilbyr fest, spillkveld og grillkveld. Alle er velkommen, håper du vil komme innom :)',
+      "YO! Jeg tilbyr fest, spillkveld og grillkveld. Alle er velkommen, håper du vil komme innom :)",
     ratings: {
       numberOfRatings: 8,
       people: 2,
@@ -14,18 +14,38 @@ export const data = [
     },
     images: [
       {
-        src: 'img/kirkegata/grille.jpg',
-        imageText: 'Grillen fyrt opp og klar for mat',
+        src: "img/kirkegata/grille.jpg",
+        imageText: "Grillen fyrt opp og klar for mat",
       },
     ],
   },
   {
-    name: 'Lerkendals heaven',
-    address: 'Klæbuveien 126',
-    capasity: 20,
-    events: ['Fest', 'Spillkveld', 'Middag'],
+    name: "Tales crib",
+    address: "Kirkegata 46A",
+    capasity: 8,
+    events: ["Fest", "Middag", "Quiz"],
     about:
-      'Hos oss er det alltid åpen dør for en liten fest, middag eller hva det skal være. Vi er 6 boys som tar livet som det faller seg og stresser ikke med noe',
+      "Kom og besøk oss på Møllenberg, vi er en hyggelig gjeng som gjerne ønsker besøk fra deg!",
+    ratings: {
+      numberOfRatings: 5,
+      people: 5,
+      appearence: 3,
+      location: 5,
+    },
+    images: [
+      {
+        src: "img/kirkegata/grille.jpg",
+        imageText: "Grillen fyrt opp og klar for mat",
+      },
+    ],
+  },
+  {
+    name: "Lerkendals heaven",
+    address: "Klæbuveien 126",
+    capasity: 20,
+    events: ["Fest", "Spillkveld", "Middag"],
+    about:
+      "Hos oss er det alltid åpen dør for en liten fest, middag eller hva det skal være. Vi er 6 boys som tar livet som det faller seg og stresser ikke med noe",
     ratings: {
       numberOfRatings: 4,
       people: 2,
@@ -34,8 +54,12 @@ export const data = [
     },
     images: [
       {
-        src: 'img/lerkendal/stue.png',
-        imageText: 'Kose med mange sittemuligheter',
+        src: "img/tale/tale1.jpg",
+        imageText: "Alltid velkommen inn!",
+      },
+      {
+        src: "img/tale/tale2.jpg",
+        imageText: "Koselig stue",
       },
     ],
   },
