257a601a57942e588b48ec9728350bec76c9420f
magnusouren <magnut0203@gmail.com>
2021-11-08 15:16:10 +0100

    Add cross to popUp-div

--- script/hybelPopUp.js
+++ script/hybelPopUp.js
@@ -9,33 +9,42 @@ function getHeightOfContent() {
     html.scrollHeight,
     html.offsetHeight
   );
-
   return height;
 }
 
 function addBackground() {
-  let background = document.createElement("div");
-  background.id = "popupBackground";
-  background.style.height = getHeightOfContent() + "px";
+  let background = document.createElement('div');
+  background.id = 'popupBackground';
+  background.style.height = getHeightOfContent() + 'px';
 
   document.body.appendChild(background);
 }
 
 function addDiv() {
-  let div = document.createElement("div");
-  div.id = "popupDiv";
+  let div = document.createElement('div');
+  div.id = 'popupDiv';
   document.body.appendChild(div);
+}
+
+function addCross() {
+  let div = document.getElementById('popupDiv');
+
+  let cross = document.createElement('b');
+  cross.id = 'closePopup';
+
+  div.appendChild(cross);
 }
 
 function createPopup() {
   addBackground();
   addDiv();
+  addCross();
 }
 
 window.onload = function () {
-  let ratingButton = document.getElementById("ratingButton");
-  let contactButton = document.getElementById("contactButton");
+  let ratingButton = document.getElementById('ratingButton');
+  let contactButton = document.getElementById('contactButton');
 
-  ratingButton.addEventListener("click", createPopup);
-  contactButton.addEventListener("click", createPopup);
+  ratingButton.addEventListener('click', createPopup);
+  contactButton.addEventListener('click', createPopup);
 };
