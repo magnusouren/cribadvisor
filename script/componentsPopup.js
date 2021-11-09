8108d29c378320119bf040bc888c66119f643da2
magnusouren <magnut0203@gmail.com>
2021-11-09 10:29:12 +0100

    Add components to create popup in new file

---
+++ script/componentsPopup.js
@@ -0,0 +1,93 @@
+function getHeightOfContent() {
+  let body = document.body;
+  let html = document.documentElement;
+
+  var height = Math.max(
+    body.scrollHeight,
+    body.offsetHeight,
+    html.clientHeight,
+    html.scrollHeight,
+    html.offsetHeight
+  );
+  return height;
+}
+
+function addBackground() {
+  let background = document.createElement('div');
+  background.id = 'popupBackground';
+  background.style.height = getHeightOfContent() + 'px';
+
+  document.body.appendChild(background);
+}
+
+function addDiv() {
+  let div = document.createElement('div');
+  div.id = 'popupDiv';
+
+  document.body.appendChild(div);
+}
+
+function addCross(popup) {
+  let cross = document.createElement('a');
+  cross.id = 'closePopup';
+
+  popup.appendChild(cross);
+}
+function addContent(popup) {
+  let content = document.createElement('div');
+  content.id = 'popupContent'; // Henvis til denne ID-en for å legge til innhold
+
+  popup.appendChild(content);
+}
+
+function exitPopup(popup, background) {
+  let content = document.getElementById('popupContent');
+
+  content.innerHTML = '';
+  popup.style.display = 'none';
+  background.style.display = 'none';
+}
+
+function addEvtLstClosePopUp(cross, popup, background) {
+  cross.addEventListener('click', function () {
+    exitPopup(popup, background);
+  });
+}
+
+export function displayPopup() {
+  let popup = document.getElementById('popupDiv');
+  let background = document.getElementById('popupBackground');
+
+  popup.style.display = 'block';
+  background.style.display = 'block';
+}
+
+function createContent() {
+  // Bruk denne for å sette inn innhold
+  let popupContent = document.getElementById('popupContent');
+  // Det under denne linja kan fjernes, ment som inspirasjon til hvordan det kan gjøres
+
+  let h1 = document.createElement('h1');
+  h1.innerText = 'Kontakt';
+
+  let p = document.createElement('p');
+  p.innerText =
+    'Her kommer det tekst avhengig av hvilken hybel du vil kontakte';
+
+  popupContent.appendChild(h1);
+  popupContent.appendChild(p);
+}
+
+export function createNewPopup() {
+  addBackground();
+  let background = document.getElementById('popupBackground');
+
+  addDiv();
+  let popup = document.getElementById('popupDiv');
+
+  addCross(popup);
+  let cross = document.getElementById('closePopup');
+
+  addContent(popup);
+  addEvtLstClosePopUp(cross, popup, background);
+}
