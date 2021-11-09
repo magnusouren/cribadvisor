a1f136bad497ebfaf2487e4b7f76e62f44445b01
magnusouren <magnut0203@gmail.com>
2021-11-09 11:46:42 +0100

    Add contact-content

--- script/hybelPopUp.js
+++ script/hybelPopUp.js
@@ -1,30 +1,65 @@
 import { createNewPopup, displayPopup } from './componentsPopup.js';
 import { fetchData, slugifyText } from './common.js';
-function getDormFromUrl() {
-  const data = fetchData();
-  const queryString = window.location.search;
-  const urlParams = new URLSearchParams(queryString);
-  const dormName = urlParams.get('dormName');
+import { getDormFromUrl } from './hybel.js';
+import { data } from '../data/data.js';
 
-  return data.find((dorm) => slugifyText(dorm.name) === dormName);
-}
-
-function createContentOr404() {
+function createContentOr404(button) {
   const dorm = getDormFromUrl();
   let container = document.getElementById('popupContent');
 
-  dorm ? createContent(container, dorm) : create404(container);
+  dorm ? createContent(container, dorm, button) : create404(container);
+}
+function createContact(container, dorm) {
+  let header = document.createElement('h3');
+  header.innerText = 'Kontakt:';
+
+  container.appendChild(header);
+
+  let phone = document.createElement('div');
+  phone.className = 'pop-up-data';
+
+  let tlf = document.createElement('b');
+  tlf.innerText = 'Tlf:';
+
+  let hostNumber = document.createElement('span');
+  hostNumber.innerText = '22 22 55 55';
+
+  phone.appendChild(tlf);
+  phone.appendChild(hostNumber);
+  container.appendChild(phone);
+
+  let email = document.createElement('div');
+  email.className = 'pop-up-data';
+
+  let epost = document.createElement('b');
+  epost.innerText = 'Epost:';
+
+  let hostEmail = document.createElement('span');
+  hostEmail.innerText = 'herkommeren@mail.com';
+
+  email.appendChild(epost);
+  email.appendChild(hostEmail);
+  container.appendChild(email);
+}
+function createRating(container, dorm) {
+  container.innerHTML = 'Stars';
 }
 
-function createContent(container, dorm) {}
+function createContent(container, dorm, button) {
+  if (button == 'contact') {
+    createContact(container, dorm);
+  } else if ((button = 'rating')) {
+    createRating(container, dorm);
+  }
+}
 
-function createPopup() {
+function createPopup(button) {
   if (document.body.contains(document.getElementById('popupDiv'))) {
     displayPopup();
-    createContentOr404();
+    createContentOr404(button);
   } else {
     createNewPopup();
-    createContentOr404();
+    createContentOr404(button);
   }
 }
 
