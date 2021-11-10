4414918cfd49e271c63a1a1325993e3494886f9c
magnusouren <magnut0203@gmail.com>
2021-11-10 21:51:18 +0100

    Add eventlisteners on buttons

--- script/hybel/hybelComponents.js
+++ script/hybel/hybelComponents.js
@@ -1,5 +1,6 @@
 import { slugifyText } from '../common.js';
 import { data } from '../../data/data.js';
+import { slideshow } from './hybelSlideshow.js';
 
 export function createDormName(container, name) {
   let dormName = document.getElementById('top-text');
@@ -21,6 +22,7 @@ export function createDormImage(container, images) {
   img.src = images[0].src;
   img.alt = images[0].imageText;
   img.className = 'dorm-image';
+  img.id = 'slideImg';
 
   slideDiv.appendChild(img);
   container.appendChild(slideDiv);
@@ -28,23 +30,29 @@ export function createDormImage(container, images) {
 export function createSlideshow(images) {
   let slideDiv = document.getElementById('dorm-slideshow');
 
+  let caption = document.createElement('div');
+  caption.id = 'slideCaption';
+  caption.innerText = images[0].imageText;
+
   let nextImg = document.createElement('div');
   nextImg.className = 'slideArrow';
   nextImg.id = 'nextImg';
+  nextImg.addEventListener('click', function () {
+    slideshow(images, 1);
+  });
   nextImg.innerText = '\u2192';
 
   let prevImg = document.createElement('div');
   prevImg.className = 'slideArrow';
   prevImg.id = 'prevImg';
+  prevImg.addEventListener('click', function () {
+    slideshow(images, -1);
+  });
   prevImg.innerText = '\u2190';
 
-  let caption = document.createElement('div');
-  caption.id = 'slideCaption';
-  caption.innerText = images[0].imageText;
-
+  slideDiv.appendChild(caption);
   slideDiv.appendChild(nextImg);
   slideDiv.appendChild(prevImg);
-  slideDiv.appendChild(caption);
 }
 
 export function createHostSection(container, host) {
