b0b6fbae4a24f9459fb4adf5361d8004050a0e4b
magnusouren <magnut0203@gmail.com>
2021-11-10 21:49:42 +0100

    Add functions to swap image and caption

---
+++ script/hybel/hybelSlideshow.js
@@ -0,0 +1,40 @@
+export function slideshow(images, direction) {
+  let slideImg = document.getElementById('slideImg');
+  let caption = document.getElementById('slideCaption');
+
+  let string = slideImg.src;
+  let splitText = string.split('/');
+  let imgSrc = splitText[splitText.length - 1];
+
+  let index = 0;
+  for (let i = 0; i < images.length; i++) {
+    if (images[i].src.includes(imgSrc)) {
+      index = i;
+      break;
+    }
+  }
+  if (direction === 1) {
+    nextImageSlideshow(images, index, slideImg, caption);
+  } else if (direction === -1) {
+    previousImageSlideshow(images, index, slideImg, caption);
+  }
+}
+
+function nextImageSlideshow(images, index, slideImg, caption) {
+  if (index + 1 === images.length) {
+    slideImg.src = images[0].src;
+    caption.innerText = images[0].imageText;
+  } else {
+    slideImg.src = images[index + 1].src;
+    caption.innerText = images[index + 1].imageText;
+  }
+}
+function previousImageSlideshow(images, index, slideImg) {
+  if (index === 0) {
+    slideImg.src = images[images.length - 1].src;
+    caption.innerText = images[images.length - 1].imageText;
+  } else {
+    slideImg.src = images[index - 1].src;
+    caption.innerText = images[index - 1].imageText;
+  }
+}
