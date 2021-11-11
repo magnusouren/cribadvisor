4bc57edf242509ca39fcecc4ba7fef058f6d439a
isaksolheim <isak.m.h.s@gmail.com>
2021-11-11 10:36:35 +0100

    Comment slideshow.js

--- script/slideshow.js
+++ script/slideshow.js
@@ -1,38 +1,66 @@
 import { data } from '../data/data.js';
 
+/**
+ * startImageSlideshow() er en funksjon som starter
+ * bilde slideshow
+ *
+ * Måten slideshowet fungerer på er som følger:
+ * Hvert 5. sekund fader slideshow-containeren ut samtidig som at
+ * bildet blir endret på. 800ms etter at containeren har fadet ut
+ * blir bildet endret på og containeren fader inn igjen.
+ */
 function startImageSlideshow() {
-  let slideshow = document.getElementById('imgSlide');
+  let slideshow = document.getElementById('imgSlide'); // Henter html element med id imgSlide
 
-  let counter = 1;
   setInterval(() => {
-    fadeOut(slideshow);
+    fadeOut(slideshow); // Kaller fadeOut funksjonen hvert 5. sekund
   }, 5000);
 
-  // Kaller på funksjon til å bytte bilde hvert 5. sekund
+  let counter = 1; // Counter som blir brukt for å vite hvilket bilde som blir vist
+
+  // Venter 800ms før setInterval funksjonen blir kallt
   setTimeout(
     () =>
       setInterval(() => {
         swapImg(data[counter], slideshow);
-        counter++;
-        if (counter == data.length) {
-          counter = 0;
-        }
+
+        // Øker counter eller setter tilbake til 0 hvis lengden er for lang
+        counter == data.length ? (counter = 0) : counter++;
       }, 5000),
     800
   );
 }
 
+/**
+ * swapImg() er en funksjon som bytter bilde i slideshowet
+ * og fader slideshow containeren inn igjen.
+ *
+ * @param {Dorm} data Hybel object fra data.js
+ * @param {HTMLElement} slideshow HTML element, containeren til slideshowet
+ */
 function swapImg(data, slideshow) {
-  slideshow.src = data.images[0].src;
-  slideshow.alt = data.images[0].imageText;
+  slideshow.src = data.images[0].src; // Setter kilde på bilde
+  slideshow.alt = data.images[0].imageText; // Setter alt tekst på bilde
+
   fadeIn(slideshow);
 }
 
+/**
+ * fadeIn() er en funksjon som endrer opacitien på slideshow containeren til maks.
+ *
+ * @param {HTMLElement} slideshow HTML element, containeren til slideshowet
+ */
 function fadeIn(slideshow) {
   slideshow.style.opacity = 1;
 }
+
+/**
+ * fadeOut() er en funksjon som endrer opacitien på slideshow containeren til 0.5
+ *
+ * @param {HTMLElement} slideshow HTML element, containeren til slideshowet
+ */
 function fadeOut(slideshow) {
   slideshow.style.opacity = 0.5;
 }
 
-startImageSlideshow();
+startImageSlideshow(); // Kaller startImageSlideshow når siden lastes inn
